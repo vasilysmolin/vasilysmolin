6820ec4b7331674f95b585c2cf4795a5f9a2f229
Василий Смолин <vasily.smolin@spiti.ru>
2019-05-13 21:21:52 +0400

    two

---
+++ app/Http/Controllers/Misc/DeployController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Misc;
+
+use App\Http\Controllers\Controller;
+
+class DeployController extends Controller
+{
+    /**
+     * ------------------------------------
+     * Deploy project from Git repository |
+     * ------------------------------------
+     * @param $requestDeployName
+     * @param $requestDeployEnv
+     * @return string|void
+     *
+     */
+
+    public function deploy($requestDeployName, $requestDeployEnv)
+    {
+        $deployName = env('DEP_NAME');
+        $deployEnv = env('DEP_ENV');
+
+        if ($requestDeployName != $deployName && $requestDeployEnv != $deployEnv) {
+            return abort(404);
+        }
+
+        $arrGitCommands = [
+            'whoami',
+            'git pull',
+            'git status',
+            'php ../artisan cache:clear',
+            'php ../artisan route:clear',
+            'php ../artisan view:clear',
+        ];
+
+        $sOutput = '';
+        foreach ($arrGitCommands as $gitCommand) {
+            $sTmp = shell_exec($gitCommand . ' 2>&1');
+            $sOutput .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">" . $gitCommand . "\n</span>";
+            $sOutput .= htmlentities(trim($sTmp)) . '<br>';
+        }
+
+        return '<pre>' . $sOutput . '</pre>';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/createDeploy/{deployName}/{deployEnv}', 'Misc\DeployController@deploy');
